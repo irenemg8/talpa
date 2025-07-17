9c08b97a6d9010fac6efe51ab02a4099aa9b548e
Vittoavril <vittoavril@gmail.com>
2025-07-17 19:23:53 +0200

    Comentario Vitto
    //Vitto

--- app/equipo/page.tsx
+++ app/equipo/page.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Linkedin, Github, Mail } from "lucide-react"
-
+//Vitto
 export default function EquipoPage() {
   const teamMembers = [
     {
